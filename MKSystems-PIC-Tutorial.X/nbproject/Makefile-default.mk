486009d6d89091b894234af7d861bcd39c69be5f
Mike Schoonover <mike@skoonie.com>
2014-11-23 00:42:56 -0700

    Updated project files.
    Signed-off-by: Mike Schoonover <mike@folsomvillage.com>

--- MKSystems-PIC-Tutorial.X/nbproject/Makefile-default.mk
+++ MKSystems-PIC-Tutorial.X/nbproject/Makefile-default.mk
@@ -86,7 +86,7 @@ ${OBJECTDIR}/_ext/1472/Tutorial-1.o: ../Tutorial-1.asm  nbproject/Makefile-${CND
 	@${MKDIR} ${OBJECTDIR}/_ext/1472 
 	@${RM} ${OBJECTDIR}/_ext/1472/Tutorial-1.o.d 
 	@${RM} ${OBJECTDIR}/_ext/1472/Tutorial-1.o 
-	@${FIXDEPS} dummy.d -e "${OBJECTDIR}/_ext/1472/Tutorial-1.err" $(SILENT) -c ${MP_AS} $(MP_EXTRA_AS_PRE) -d__DEBUG -d__MPLAB_DEBUGGER_PK3=1 -q -p$(MP_PROCESSOR_OPTION)  -l\"${OBJECTDIR}/_ext/1472/Tutorial-1.lst\" -e\"${OBJECTDIR}/_ext/1472/Tutorial-1.err\" $(ASM_OPTIONS)   -o\"${OBJECTDIR}/_ext/1472/Tutorial-1.o\" \"../Tutorial-1.asm\" 
+	@${FIXDEPS} dummy.d -e "${OBJECTDIR}/_ext/1472/Tutorial-1.err" $(SILENT) -c ${MP_AS} $(MP_EXTRA_AS_PRE) -d__DEBUG  -q -p$(MP_PROCESSOR_OPTION)  -l\"${OBJECTDIR}/_ext/1472/Tutorial-1.lst\" -e\"${OBJECTDIR}/_ext/1472/Tutorial-1.err\" $(ASM_OPTIONS)   -o\"${OBJECTDIR}/_ext/1472/Tutorial-1.o\" \"../Tutorial-1.asm\" 
 	@${DEP_GEN} -d "${OBJECTDIR}/_ext/1472/Tutorial-1.o"
 	@${FIXDEPS} "${OBJECTDIR}/_ext/1472/Tutorial-1.o.d" $(SILENT) -rsi ${MP_AS_DIR} -c18 
 	
@@ -106,7 +106,7 @@ endif
 ifeq ($(TYPE_IMAGE), DEBUG_RUN)
 dist/${CND_CONF}/${IMAGE_TYPE}/MKSystems-PIC-Tutorial.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}: ${OBJECTFILES}  nbproject/Makefile-${CND_CONF}.mk    
 	@${MKDIR} dist/${CND_CONF}/${IMAGE_TYPE} 
-	${MP_LD} $(MP_EXTRA_LD_PRE)   -p$(MP_PROCESSOR_OPTION)  -w -x -u_DEBUG -z__ICD2RAM=1 -m"${DISTDIR}/${PROJECTNAME}.${IMAGE_TYPE}.map"   -z__MPLAB_BUILD=1  -z__MPLAB_DEBUG=1 -z__MPLAB_DEBUGGER_PK3=1 $(MP_LINKER_DEBUG_OPTION) -odist/${CND_CONF}/${IMAGE_TYPE}/MKSystems-PIC-Tutorial.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}  ${OBJECTFILES_QUOTED_IF_SPACED}     
+	${MP_LD} $(MP_EXTRA_LD_PRE)   -p$(MP_PROCESSOR_OPTION)  -w -x -u_DEBUG -z__ICD2RAM=1 -m"${DISTDIR}/${PROJECTNAME}.${IMAGE_TYPE}.map"   -z__MPLAB_BUILD=1  -z__MPLAB_DEBUG=1 $(MP_LINKER_DEBUG_OPTION) -odist/${CND_CONF}/${IMAGE_TYPE}/MKSystems-PIC-Tutorial.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}  ${OBJECTFILES_QUOTED_IF_SPACED}     
 else
 dist/${CND_CONF}/${IMAGE_TYPE}/MKSystems-PIC-Tutorial.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}: ${OBJECTFILES}  nbproject/Makefile-${CND_CONF}.mk   
 	@${MKDIR} dist/${CND_CONF}/${IMAGE_TYPE} 
