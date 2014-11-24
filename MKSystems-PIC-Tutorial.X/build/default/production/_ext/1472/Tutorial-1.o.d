8694712ade238149405f6ccc9bb8c30cc89c5662
Hunter Schoonover <hunter@skoonie.com>
2014-11-23 22:07:12 -0700

    Updated project files.
    Signed-off-by: Hunter Schoonover <hunter@skoonie.com>

--- MKSystems-PIC-Tutorial.X/build/default/production/_ext/1472/Tutorial-1.o.d
+++ MKSystems-PIC-Tutorial.X/build/default/production/_ext/1472/Tutorial-1.o.d
@@ -1,4 +1,4 @@
 build/default/production/_ext/1472/Tutorial-1.o:  \
-	C:/Users/Mike/Documents/6\ -\ Controlled\ Documents/PIC\ Projects\ Git\ Repositories/MKSystems-PIC-Tutorial/Tutorial-1.asm  \
-	C:/Users/Mike/Documents/6\ -\ Controlled\ Documents/PIC\ Projects\ Git\ Repositories/MKSystems-PIC-Tutorial/p16f1459.inc  \
+	C:/Users/Hunter/Documents/PIC\ Projects/MKSystems-PIC-Tutorial/Tutorial-1.asm  \
+	C:/Users/Hunter/Documents/PIC\ Projects/MKSystems-PIC-Tutorial/p16f1459.inc  \
 
