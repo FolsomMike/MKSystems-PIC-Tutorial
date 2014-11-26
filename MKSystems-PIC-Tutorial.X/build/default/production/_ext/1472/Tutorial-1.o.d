79a15589dc70b6cc412d0df20787d52bfe1c1f35
Mike Schoonover <mike@skoonie.com>
2014-11-25 19:43:13 -0700

    Updated project files after updating IDE to v2.26.
    Signed-off-by: Mike Schoonover <mike@skoonie.com>

--- MKSystems-PIC-Tutorial.X/build/default/production/_ext/1472/Tutorial-1.o.d
+++
@@ -1,4 +0,0 @@
-build/default/production/_ext/1472/Tutorial-1.o:  \
-	C:/Users/Hunter/Documents/PIC\ Projects/MKSystems-PIC-Tutorial/Tutorial-1.asm  \
-	C:/Users/Hunter/Documents/PIC\ Projects/MKSystems-PIC-Tutorial/p16f1459.inc  \
-
